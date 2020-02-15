6e4b1f7ab7c4b26daef53136e31e3efe5d864f39
Saad <m.saad.raza1999@gmail.com>
2020-02-15 15:59:04 -0500

    my changes

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ var app = express();
 // express looks for ejs files from the ./views folder
 app.get("/", function(req, res){
     name = 'husni';
-    res.render("home.ejs", {nameEJS: name});
+    //res.render("home.ejs", {nameEJS: name});
+    //res.render("usegoogle2.ejs");
+    res.render("google_with_rec.ejs");
+
 })
 app.listen(3000, function(){
     console.log("Sever is listening!");
